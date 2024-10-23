a6bf1a1f8e80f665ba0c1b7ba88e2f34e554119d
GarayOcs <inolga17@gmail.com>
2024-10-23 10:47:32 -0600

    Initial commit

---
+++ app/src/main/java/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.personaldataapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Aquí vamos a agregar la lógica de búsqueda
+    }
+}
